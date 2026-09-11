c7e0c39c6bdaf0ffd97c5ee0eaba8c370d1a55a0
NJ <njglobalcommerce@gmail.com>
2026-09-11 00:18:39 -0600

    added cors origins

--- backend/nest/src/main.ts
+++ backend/nest/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { allowedOrigins } from './config/cors-origins';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
+  });
 
   app.setGlobalPrefix('api');
 
