c9238a5530db780fea26dabdaaa358ac2d7be8d3
NJ <njglobalcommerce@gmail.com>
2026-09-12 18:02:57 -0600

    Improve button design

--- frontend/next/app/tratos/partials/CreateLeadForm.tsx
+++ frontend/next/app/tratos/partials/CreateLeadForm.tsx
@@ -10,7 +10,7 @@ export const CreateLeadForm = ({ onSubmit }: { onSubmit: (values: CreateLeadDto)
         <button
             type="button"
             onClick={() => setIsNewLeadModalOpen(true)}
-            className="bg-(--gray-2) text-white px-4 rounded-lg hover:bg-white hover:text-(--gray-2) hover:border hover:border-(--gray-2) transition cursor-pointer"
+            className="bg-black text-white h-12 self-end px-4 rounded-lg hover:bg-white hover:text-black hover:border hover:border-black transition cursor-pointer"
         >
             Nuevo Lead
         </button>
@@ -64,7 +64,7 @@ export const CreateLeadForm = ({ onSubmit }: { onSubmit: (values: CreateLeadDto)
                                 </label>
                                 <div className="mt-2 flex justify-end gap-3">
                                     <button type="button" onClick={() => setIsNewLeadModalOpen(false)} className="cursor-pointer rounded-lg px-4 py-2 border border-(--gray-2)">Cancelar</button>
-                                    <button type="submit" disabled={isSubmitting} className="cursor-pointer rounded-lg bg-(--gray-2) px-4 py-2 text-white">Guardar</button>
+                                    <button type="submit" disabled={isSubmitting} className="cursor-pointer rounded-lg bg-black px-4 py-2 text-white">Guardar</button>
                                 </div>
                             </Form>
                         )}
